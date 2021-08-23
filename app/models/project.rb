038dfccbfcf36704cf21d04e9b1855d14c4a8285
VictorNain26 <victor.lenain26@gmail.com>
2021-08-23 22:20:51 +0200

    add routes a discuter

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :voice_records
+  has_one :note
 
   validates :title, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :projects, only: [:index, :create, :show, :update, :destroy] do
-    resources :notes, only: [:update]
+  resources :projects, except: %i[edit new] do
+    patch :note
+    resources :voicerecords, only: %i[create index destroy]
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
