f5c01e95b5526baafc25fdc7ae258b3f99a10dff
issam hadjal <issam.hadjal@gmail.com>
2021-08-24 14:36:34 +0200

    add show method and begin view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,16 @@
 class ProjectsController < ApplicationController
+    before_action :set_project, only: [:show]
+
+    def show
+        @voice_record = VoiceRecord.new
+        @note = Note.new
+    end
+
+    private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
 end
+
