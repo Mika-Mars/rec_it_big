f64ca0a7907c0f2ccf7aa07a4c5ae9c6e55343a8
VictorNain26 <victor.lenain26@gmail.com>
2021-08-24 14:07:31 +0200

    start add project button

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,17 @@
 class ProjectsController < ApplicationController
+  def create
+    @project = Project.new(project_params)
+    @projet.user_id = current_user.id
+    if @project.save!
+      redirect_to projects_path
+    else
+      render projects_path
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title)
+  end
 end
