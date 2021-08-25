4773214828b024c82d5331e7f422615abc61c5b2
sacha bojanowski <bojanowski.sacha@gmail.com>
2021-08-25 17:27:56 +0200

    mm

--- app/javascript/components/wavesurfer.js
+++ app/javascript/components/wavesurfer.js
@@ -2,11 +2,10 @@ const wave_surfer = WaveSurfer.create({
     container: '#waveform',
     waveColor: 'violet',
     progressColor: 'purple'
-  
-    
-    
+
+
+
   });
   wave_surfer.on('ready', function () {
     wave_surfer.play();
   });
-  wave_surfer.load('http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3');
