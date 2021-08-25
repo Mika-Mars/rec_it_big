ee8f641ec97f59ea1ab66eac8fe3ba76030f5eba
VictorNain26 <victor.lenain26@gmail.com>
2021-08-25 17:25:27 +0200

    fixes

--- app/javascript/components/get_audio.js
+++ app/javascript/components/get_audio.js
@@ -4,34 +4,39 @@ const getAudio = () => {
     audio: true
   };
   let chunks = [];
-
-  navigator.mediaDevices.getUserMedia(constraints)
-    .then((stream) => {
-      const mediaRecorder = new MediaRecorder(stream);
-      recButton.addEventListener('click', () => {
-        if (mediaRecorder.state === "inactive") {
-          mediaRecorder.start()
-          console.log(mediaRecorder.state);
-          mediaRecorder.ondataavailable = (e) => {
-            chunks.push(e.data);
+  if (recButton) {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia(constraints)
+        .then((stream) => {
+          const mediaRecorder = new MediaRecorder(stream);
+          recButton.addEventListener('click', () => {
+            if (mediaRecorder.state === "inactive") {
+              mediaRecorder.start()
+              console.log(mediaRecorder.state);
+              mediaRecorder.ondataavailable = (e) => {
+                chunks.push(e.data);
+              }
+            } else {
+              mediaRecorder.stop();
+              console.log(mediaRecorder.state);
+            }
+          })
+          mediaRecorder.onstop = function (e) {
+            const blob = new Blob(chunks, {
+              'type': 'audio/wav; codecs=MS_PCM'
+            });
+            chunks = [];
+            const audioURL = window.URL.createObjectURL(blob);
+            console.log(audioURL);
           }
-        } else {
-          mediaRecorder.stop();
-          console.log(mediaRecorder.state);
-        }
-      })
-      mediaRecorder.onstop = function (e) {
-        const blob = new Blob(chunks, {
-          'type': 'audio/wav; codecs=MS_PCM'
-        });
-        chunks = [];
-        const audioURL = window.URL.createObjectURL(blob);
-        console.log(audioURL);
-      }
-    })
-    .catch((err) => {
-      console.log("error" + err);
-    })
+        })
+        .catch((err) => {
+          console.log("error" + err);
+        })
+    } else {
+      console.log('getUserMedia not supported on your browser!');
+    }
+  }
 }
 
 export {
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -36,4 +36,3 @@ document.addEventListener('turbolinks:load', () => {
   addNewProject();
   getAudio();
 });
-
