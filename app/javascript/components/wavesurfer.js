a5b20b3cb6d4d1743b0c3e416da1484f9e3d0fd3
issam hadjal <issam.hadjal@gmail.com>
2021-08-30 14:47:20 +0200

    height wave align notes

--- app/javascript/components/wavesurfer.js
+++ app/javascript/components/wavesurfer.js
@@ -21,14 +21,11 @@ const initWavesurfer = () => {
         barWidth: 3,
         barHeight: 1,
         barGap: 1,
-        backend: 'MediaElement'
-        height: 1500,
         progressColor: 'white',
         backend: 'MediaElement',
         autoCenter: true,
         responsive: true,
         scrollParent: true,
-
       });
       play_wave.addEventListener("click", (event) => {
         const isPlaying = event.currentTarget.dataset.playing === "true";
