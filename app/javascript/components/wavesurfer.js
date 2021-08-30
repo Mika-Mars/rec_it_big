24a3ea82bfeac3412204c9b3d8bed4599640610a
VictorNain26 <victor.lenain26@gmail.com>
2021-08-30 17:27:49 +0200

    test voice record SYNC

--- app/javascript/components/wavesurfer.js
+++ app/javascript/components/wavesurfer.js
@@ -1,15 +1,10 @@
 import WaveSurfer from 'wavesurfer.js';
 
 const initWavesurfer = () => {
-  const play_wave = document.querySelector("#btn_play");
-  const stop_wave = document.getElementById("btn_stop");
   const container = document.querySelector("#waveform");
-  const voices = document.querySelectorAll(".voices");
-
   if (container) {
-
     if (container.dataset.instru) {
-      const pause_wave = document.getElementById("btn_pause");
+      const play_wave = document.querySelector("#btn_play");
       const stop_wave = document.getElementById("btn_stop");
       const voices = document.querySelectorAll(".voices");
 
@@ -27,57 +22,59 @@ const initWavesurfer = () => {
         responsive: true,
         scrollParent: true,
       });
+      wave_surfer.load(container.dataset.instru);
       play_wave.addEventListener("click", (event) => {
         const isPlaying = event.currentTarget.dataset.playing === "true";
-        const pauseIcon = document.querySelector("#pause-icon")
         const playIcon = document.querySelector("#play-icon")
+        const audioInstru = document.querySelector('#waveform audio');
 
         if (isPlaying) {
           event.currentTarget.dataset.playing = "false";
           wave_surfer.pause();
-          // pauseIcon.classList.add("d-none");
-          // playIcon.classList.remove("d-none");
           playIcon.classList.remove("fa-pause");
           playIcon.classList.add("fa-play");
-
+          voices.forEach(voice => {
+            voice.pause();
+            voice.currentTime = 0;
+          })
         } else {
           event.currentTarget.dataset.playing = "true";
           wave_surfer.play();
           playIcon.classList.remove("fa-play");
           playIcon.classList.add("fa-pause");
+          voices.forEach(voice => {
+            if (voice.dataset.start >= audioInstru.currentTime) {
+              setInterval(() => {
+                voice.start();
+              }, audioInstru.currentTime - voice.dataset.start);
+              if (voice.dataset.start <= audioInstru.currentTime && audioInstru.currentTime <= voice.dataset.end) {
+
+              }
+            }
+          })
         }
-
       });
-
       stop_wave.addEventListener("click", (event) => {
         wave_surfer.stop();
-      });
-      wave_surfer.load(container.dataset.instru);
-
-      wave_surfer.on('audioprocess', () => {
-        voices.forEach(voice => {
-          if (voice.dataset.start == Math.round(wave_surfer.getCurrentTime())) {
-            voice.play();
-          }
-        })
-      })
-      wave_surfer.on('click', () => {
-        voices.forEach(voice => {
-          if (voice.dataset.start <= wave_surfer.getCurrentTime() && wave_surfer.getCurrentTime() <= voice.dataset.end) {
-            voice.currentTime = wave_surfer.getCurrentTime() - voice.dataset.start;
-            voice.play();
-          } else {
-            voice.pause();
-            voice.currentTime = 0;
-          }
-        })
-      })
-      wave_surfer.on('pause', () => {
         voices.forEach(voice => {
           voice.pause();
           voice.currentTime = 0;
         })
-      })
+      });
+
+      // A GARDER SVP
+
+      // wave_surfer.on('click', () => {
+        //   voices.forEach(voice => {
+      //     if (voice.dataset.start <= wave_surfer.getCurrentTime() && wave_surfer.getCurrentTime() <= voice.dataset.end) {
+      //       voice.currentTime = wave_surfer.getCurrentTime() - voice.dataset.start;
+      //       voice.play();
+      //     } else {
+      //       voice.pause();
+      //       voice.currentTime = 0;
+      //     }
+      //   })
+      // })
     }
   }
 }
