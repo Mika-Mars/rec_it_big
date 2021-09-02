014f7fda78404b105909b9c7ecf32dc3daceb563
sacha bojanowski <bojanowski.sacha@gmail.com>
2021-09-02 10:45:33 +0200

    repare btn

--- app/javascript/components/get_audio.js
+++ app/javascript/components/get_audio.js
@@ -29,6 +29,7 @@ const getAudio = () => {
           const instruCurrentTime = document.querySelector('#waveform audio');
           const mediaRecorder = new MediaRecorder(stream);
           recButton.addEventListener('click', () => {
+            console.log("sacha")
             document.getElementById("rec-btn").classList.toggle('fa-record-vinyl-playing');
             if (mediaRecorder.state === "inactive") {
               mediaRecorder.start();
